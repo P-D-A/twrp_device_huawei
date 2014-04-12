fef54da1ad204e541fdf3a772e05ba3071c7e396
ShevT <shev_t@mail.ru>
2014-04-12 09:37:54 +0400

    all in one

--- front/device_front.mk
+++ front/device_front.mk
@@ -5,3 +5,8 @@ $(call inherit-product, build/target/product/full.mk)
 PRODUCT_CHARACTERISTICS      := default
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_LOCALES              := en_US
+
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+$(call inherit-product, hardware/ti/omap4xxx/omap4.mk)
