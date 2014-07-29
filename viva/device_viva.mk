d1cbe5edbbb3406767ae1dd2bf9a96155e8b8483
ShevT <shev_t@mail.ru>
2014-07-29 13:54:43 +0400

    device_viva.mk: added AAPT_CONFIG

--- viva/device_viva.mk
+++ viva/device_viva.mk
@@ -5,3 +5,8 @@ $(call inherit-product, build/target/product/full.mk)
 PRODUCT_CHARACTERISTICS      := default
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_LOCALES              := en_US
+
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+$(call inherit-product, hardware/ti/omap4xxx/omap4.mk)
