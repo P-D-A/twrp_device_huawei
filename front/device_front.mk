631af598a0378c3ef1d49c9378fd03b84eeddfea
ShevT <shev_t@mail.ru>
2014-07-31 08:42:46 +0400

    added f2fs utilities

--- front/device_front.mk
+++ front/device_front.mk
@@ -2,6 +2,15 @@
 # Necessary to mount a PC
 $(call inherit-product, build/target/product/full.mk)
 
+# Ramdisk
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/fstab.front:root/fstab.front
+
+# f2fs utilities
+PRODUCT_PACKAGES += \
+    fs_mgr \
+    resize2fs
+
 PRODUCT_CHARACTERISTICS      := default
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_LOCALES              := en_US
