eaba9894d361111bec1ef5e1819b4d28f80f15ed
ShevT <shev_t@mail.ru>
2015-02-19 21:36:24 +0300

    added viva (U9200) tree

---
+++ viva/omni_viva.mk
@@ -0,0 +1,12 @@
+# Inherit device configuration
+$(call inherit-product, device/huawei/viva/device_viva.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := viva
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := viva
+PRODUCT_NAME := omni_viva
+PRODUCT_BRAND := Huawei
+PRODUCT_MODEL := U9200
+PRODUCT_MANUFACTURER := Huawei
