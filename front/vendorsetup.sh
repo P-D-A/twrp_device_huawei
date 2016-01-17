ee77373039c04428c1d2b38757239feeb8795105
ShevT <shev_t@mail.ru>
2016-01-17 10:08:35 +0300

    added TWRP patch. Fixed some problems

--- front/vendorsetup.sh
+++ front/vendorsetup.sh
@@ -21,3 +21,4 @@
 # function: add_lunch_combo generic-eng
 
 add_lunch_combo omni_front-eng
+sh device/huawei/front/res/applypatch.sh
--- viva/vendorsetup.sh
+++ viva/vendorsetup.sh
@@ -21,3 +21,4 @@
 # function: add_lunch_combo generic-eng
 
 add_lunch_combo omni_viva-eng
+sh device/huawei/front/res/applypatch.sh
